120ddce63bd108da80d277b2322a00be9990707d
Gustavo Passos <gustavo.passos@lambda3.com.br>
2020-01-25 10:48:42 -0300

    add input handler to control character

---
+++ Assets/Scripts/Controller/InputHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controller
+{
+    public class InputHandler : MonoBehaviour
+    {
+        [SerializeField] private CharacterController character;
+
+        private void Update()
+        {
+            float xInput = Input.GetAxisRaw("Horizontal");
+
+            // Jumping
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                character.OnJumpButtonDown();
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                character.OnJumpButtonUp();
+            }
+
+            character.SetXInput(xInput);
+        }
+    }
+}
